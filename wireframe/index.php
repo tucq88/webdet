c801a6a233c2a180ca1d66c5c7130859640e21d1
Tu Chu Quang <tucq88@gmail.com>
2013-12-23 18:25:23 +0700

    setup library + index

---
+++ wireframe/index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<title>Web Development Essential Tools</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/bootstrap.css" />
+<link rel="stylesheet" href="css/font-awesome.css" />
+</head>
+<body class="container">
+    <header>
+        <h1>
+            <a href="">Hello World</a>
+        </h1>
+        <p class="lead">
+            <small>A tiny introduction</small>
+        </p>
+    </header>
+    <nav class="navbar navbar-default">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="">Menu 1</a></li>
+            <li><a href="">Menu 2</a></li>
+            <li><a href="">Menu 3</a></li>
+            <li><a href="">Menu 4</a></li>
+        </ul>
+    </nav>
+    <div id="main-content">
+        <div class="row">
+            <div class="col-lg-2">
+                
+            </div>
+        </div>
+    </div>
+    <footer> </footer>
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 10]>
+    <script type="text/javascript" src="js/html5shiv.js"></script>
+    <script type="text/javascript" src="js/jquery.placeholder.min.js"></script>
+    <script type="text/javascript">
+        $(function() {
+            $('input, textarea').placeholder();
+        });
+    </script>
+    <![endif]-->
+</body>
+</html>
